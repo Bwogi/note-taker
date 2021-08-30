499f2b7fce9d96e7148dff6d66562712a6704e43
Andrew Bwogi <andrew.bwogi@gmail.com>
2021-08-30 04:24:12 -0700

    server running

---
+++ server.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
+  });
+  
